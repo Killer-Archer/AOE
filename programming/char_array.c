711b81175ce2ab6b7c893b5219faf769385cbca4
DarkArrow <gajendrar043@gmail.com>
2023-03-05 06:09:34 -0500

    Branching

---
+++ programming/char_array.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+char str_a[20];
+str_a[0] = 'H';
+str_a[1] = 'E';
+str_a[2] = 'L';
+str_a[3] = 'L';
+str_a[4] = 'O';
+str_a[5] = ',';
+str_a[6] = ' ';
+str_a[7] = 'W';
+str_a[8] = 'O';
+str_a[9] = 'R';
+str_a[10] = 'L';
+str_a[11] = 'D';
+str_a[12] = '!';
+str_a[13] = '\n';
+str_a[14] = 0;
+printf(str_a);
+}
